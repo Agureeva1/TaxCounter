e160016d8b91be1a6dd8107edd7b080c0518f565
Agureeva Anna <agureeva@ro.ru>
2024-02-07 16:17:18 +0300

    Finish

--- src/test/java/LenaProgect/Idea.java
+++
@@ -1,9 +0,0 @@
-package LenaProgect;
-
-import java.time.LocalDate;
-
-public class Idea {
-    //TODO каждая схемы должна иметь дату создания, для этого нужно использовать класс LocalDate
-    LocalDate date = LocalDate.now();
-
-}
